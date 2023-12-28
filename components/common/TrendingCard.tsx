286a55d7025e81d33c66cc1574c4f76625e1a0a0
jbrtsmbrr <jbrt.dev@gmail.com>
2023-12-28 17:57:05 +0800

    init commit

--- components/common/TrendingCard.tsx
+++ components/common/TrendingCard.tsx
@@ -20,6 +20,7 @@ const TrendingCard = ({ name, imageUrl }: TrendingCardProps) => {
   const textSize = checkText(name) ? 'text-7xl' : 'text-8xl';
   const textScaleSize = checkText(name) ? 'group-hover:text-5xl' : 'group-hover:text-7xl';
   return (
+    // @ts-ignore
     <div style={{ '--image-url': `url(${imageUrl})` }} className={`group relative cursor-pointer overflow-hidden bg-[image:var(--image-url)] aspect-square h-full w-full bg-[length:100%_100%] hover:bg-[length:120%_120%] transition-all duration-500 ease-in-out bg-center bg-no-repeat`}>
     <div className='bg-opacity-15 bg-purple-300 group-hover:bg-purple-700 group-hover:bg-opacity-20 h-full w-full transition-all duration-500 ease-in-out' />
       {/* <Image alt={`trending-${name}`} src={imageUrl} height={500} width={500} className='group-hover:scale-150  transition-all duration-500 ease-in-out' /> */}
