835ac43632b04ef130e34eaa012e0b7234995f71
jbrtsmbrr <jbrt.dev@gmail.com>
2024-01-15 12:11:36 +0800

    added: debounce utility function

---
+++ utils/debounce.ts
@@ -0,0 +1,12 @@
+export const debounce = (fn, delay) => {
+  let timeout;
+
+  return (...args) => {
+    if (timeout) clearTimeout(timeout)
+
+
+    timeout = setTimeout(() => {
+      fn(...args)
+    }, delay)
+  }
+}
