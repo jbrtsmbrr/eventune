c7bf4f7d82b183275ddd1df498dd1f1f900601df
jbrtsmbrr <jbrt.dev@gmail.com>
2024-01-15 12:14:08 +0800

    added: spotify api in config

--- next.config.js
+++ next.config.js
@@ -3,14 +3,18 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-      protocol: 'https',
-      hostname: "encrypted-tbn0.gstatic.com"
-    },
-    {
-      protocol: "https",
-      hostname: "utfs.io"
-    }
-  ]
+        protocol: 'https',
+        hostname: "encrypted-tbn0.gstatic.com"
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io" // Uploadthing
+      },
+      {
+        protocol: "https",
+        hostname: "i.scdn.co" // Spotify API
+      },
+    ]
   },
   env: {
     NEXT_PUPLIC_SPOTIFY_CLIENT_ID: process.env.NEXT_PUPLIC_SPOTIFY_CLIENT_ID,
