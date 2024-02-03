2c5f2bbc6db7ae552ed894bd59eb1b88c068052a
jbrtsmbrr <jbrt.dev@gmail.com>
2024-02-04 00:49:47 +0800

    updated eventune logo

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -6,9 +6,12 @@ import { Button } from "../ui/button";
 
 const Header = () => {
   return (
-    <div className="fixed top-0 z-10 flex items-center w-full p-4 py-2 justify-evenly bg-[#000000] text-gray-100 bg-opacity-60 backdrop-blur-3xl">
+    <div className="fixed top-0 z-50 flex items-center w-full p-4 py-2 justify-evenly bg-[#000000] text-gray-100 bg-opacity-10 backdrop-blur-3xl">
       <Link href="/">
-        <Image alt="eventune-logo" src="/assets/icons/eventune-high-resolution-logo-white-transparent.png" width={45} height={45} />
+        <div className="flex items-center gap-4">
+          <Image alt="eventune-logo" src="/assets/icons/eventune-high-resolution-logo-white-transparent.png" width={45} height={45} />
+          <p className="text-white font-bold font-serif tracking-widest text-sm hidden lg:block">eventune.</p>
+        </div>
       </Link>
       <div className="p-2 px-4 flex gap-4 rounded-full outline-none focus-within:ring-2 ring-gray-800 shadow-lg bg-white md:w-1/5 w-3/5">
         {/* <Image alt="searc-icon" src="/assets/icons/icons8-search.svg" width={14} height={14} /> */}
