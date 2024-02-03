f497a8d95894ad57322946af00a815de6ef6a0f7
jbrtsmbrr <jbrt.dev@gmail.com>
2024-02-04 00:50:29 +0800

    added test fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { Inter, Roboto, Poppins, Lato, Playfair_Display, Nunito_Sans } from 'next/font/google'
+import { Inter, Roboto, Poppins, Lato, Playfair_Display, Nunito_Sans, DM_Serif_Display, Cormorant_Garamond } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from "@clerk/nextjs";
 
@@ -9,6 +9,8 @@ const poppins = Poppins({ weight: '400', subsets: ["latin"] })
 const lato = Lato({ weight: '400', subsets: ["latin"] })
 const playfairDisplay = Playfair_Display({ subsets: ["latin"] })
 const nunitoSans = Nunito_Sans({ subsets: ["cyrillic"] })
+const dmSerifDisplay = DM_Serif_Display({ weight: "400", subsets: ['latin'] })
+const cormorantGaramond = Cormorant_Garamond({ weight: '400', subsets: ['latin', 'vietnamese', 'cyrillic', 'cyrillic-ext'] })
 
 export const metadata: Metadata = {
   title: 'Eventune',
@@ -29,6 +31,8 @@ export default function RootLayout({
         {/* <body className={`${lato.className}`}>{children}</body> */}
         {/* <body className={`${playfairDisplay.className}`}>{children}</body> */}
         <body className={`${nunitoSans.className} bg-black`}>{children}</body>
+        {/* <body className={`${dmSerifDisplay.className} bg-black`}>{children}</body> */}
+        {/* <body className={`${cormorantGaramond.className} bg-black`}>{children}</body> */}
       </html>
     </ClerkProvider>
 
