b992a7205a91429d83983a06d45db6a1b125078c
jbrtsmbrr <jbrt.dev@gmail.com>
2024-02-07 22:17:18 +0800

    init footer

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,13 @@
 import Header from '@/components/common/Header'
 import React from 'react'
+import Footer from './Footer'
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
       <Header />
       {children}
+      <Footer />
     </main>
   )
 }
