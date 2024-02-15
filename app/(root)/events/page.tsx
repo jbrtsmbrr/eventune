0e1eaebfab683afbf037a9bdc3c34695c8d6515d
jbrtsmbrr <jbrt.dev@gmail.com>
2024-02-15 23:27:03 +0800

    init event filters

--- app/(root)/events/page.tsx
+++ app/(root)/events/page.tsx
@@ -2,19 +2,38 @@ import EventCard from '@/components/common/EventCard'
 import { getAllEvents } from '@/lib/database/actions/event.action';
 import { IEvent } from '@/lib/types/event';
 import React from 'react'
+import FilterForm from './FilterForm';
+import moment from 'moment';
 
-const Events = async ({ searchParams }: { searchParams: { limit: number } }) => {
-  console.log(searchParams)
+export interface IEventProps {
+  searchParams: {
+    limit: number,
+    from: string,
+    to: string
+  }
+};
+
+const validateDate = (from: string, to: string) => {
+  if (!from || !to) return null;
+
+  if (!moment(from, ["YYYY-MM-DD"]).isValid() && !moment(to, ["YYYY-MM-DD"]).isValid()) return null;
+
+  return {
+    from,
+    to
+  }
+}
+
+const Events = async ({ searchParams }: IEventProps) => {
   const filters = {
-    limit: searchParams.limit ?? 0
+    limit: searchParams.limit ?? 0,
+    dateRange: validateDate(searchParams.from, searchParams.to) 
   }
   const events: IEvent[] | null = await getAllEvents(filters);
-  console.log(events)
+
   return (
     <div className="relative top-[61px] left-1/2 -translate-x-1/2 md:p-8 p-14 flex flex-col items-center justify-center bg-[#121212] mb-[61px]">
-      <div>
-
-      </div>
+      <FilterForm />
       {events
         ?
         <div className="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 w-full gap-4">
