a9003178afab742d5daed283df9d3b4cdc406607
jbrtsmbrr <jbrt.dev@gmail.com>
2024-02-22 00:06:35 +0800

    updated base button component

--- components/common/BaseButton.tsx
+++ components/common/BaseButton.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Button } from '../ui/button'
 
-const BaseButton = ({ children }: { children?: React.ReactNode }) => {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> { 
+  children?: React.ReactNode 
+}
+
+const BaseButton = ({ children, className, ...buttonProps }: ButtonProps) => {
   return (
-    <Button className="uppercase font-serif px-4 py-2 shadow-lg bg-white border-2 border-white hover:bg-opacity-25 rounded-none text-black hover:text-white transition-all duration-500">{children}</Button>
+    <Button className={`uppercase px-4 py-2 shadow-lg bg-white border-2 border-white hover:bg-black hover:bg-opacity-25 rounded-none text-black hover:text-gray-200 transition-all duration-500 font-bold tracking-wider ${className}`} {...buttonProps}>{children}</Button>
   )
 }
 
